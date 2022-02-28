09b89bedb00e550f79fcd14182a7b8bd74d1bcc6
tejas96 <tejas96patil@gmail.com>
2022-02-28 21:21:20 +0530

    add prettier, eslint config and provide paper theme

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import AmbulanceApp from './src/navigation';
 import {AuthProvider} from './src/providers';
 import {Provider as PaperProvider} from 'react-native-paper';
+import {PaperTheme} from './src/config';
 const App = () => {
   return (
-    <PaperProvider>
+    <PaperProvider theme={PaperTheme}>
       <AuthProvider>
         <AmbulanceApp />
       </AuthProvider>
