fbe69c52e49a5a0f9de2bfdcc730db8253b6fe31
tejas96 <tejas96patil@gmail.com>
2022-03-05 23:36:34 +0530

    add checkbox common component and update button component theme

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,8 @@ export {default as Loader} from './loader';
 export {default as Chip} from './Chip';
 export {default as DropDown} from './DropDown';
 export {default as MapView} from './MapView';
+export {default as CheckBox} from './CheckBox';
+export {default as MultiSelectBox} from './MultiSelectBox';
 export {
   default as Typography,
   Caption,
