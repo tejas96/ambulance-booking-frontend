515abb4a35e4efb136bd463a6f8c07addf2ea74b
tejas96 <tejas96patil@gmail.com>
2022-03-19 15:35:54 +0530

    add history page and setting page

--- src/screens/setting/index.tsx
+++ src/screens/setting/index.tsx
@@ -6,6 +6,10 @@ import {useLoggedInUser} from '../../hooks';
 import styles from './styles';
 import Feather from 'react-native-vector-icons/Feather';
 import useSetting from './container';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {AppStackParamList} from '../../navigation/AppStackParamsList';
+import * as Routes from '../../navigation/routes';
 
 interface SettingItem {
   title: string;
@@ -28,7 +32,9 @@ const SettingItem: React.FC<SettingItem> = ({
           <Feather style={styles.endIcon} name={icon} size={25} />
           <Text style={[styles.title, titleStyle]}>{title}</Text>
         </View>
-        <Feather style={styles.endIcon} name={endIcon} size={20} />
+        {endIcon ? (
+          <Feather style={styles.endIcon} name={endIcon} size={20} />
+        ) : null}
       </View>
     </Pressable>
   );
@@ -38,6 +44,8 @@ interface IProps {}
 const Setting: React.FC<IProps> = () => {
   const {handleLogout} = useSetting();
   const {user} = useLoggedInUser();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<AppStackParamList>>();
   return (
     <SafeAreaView>
       <View style={styles.container}>
@@ -50,7 +58,9 @@ const Setting: React.FC<IProps> = () => {
         </View>
         <Divider />
         <SettingItem
-          onPress={() => {}}
+          onPress={() => {
+            navigation.navigate(Routes.ABOUT);
+          }}
           title="About"
           icon="info"
           endIcon="chevron-right"
