9c5c9897d48598eb2941c61dcad46b8a12f81318
tejas96 <tejas96patil@gmail.com>
2022-03-25 17:52:19 +0530

    add socket connection in socket provider

--- src/providers/SocketProvider.tsx
+++ src/providers/SocketProvider.tsx
@@ -1,6 +1,9 @@
 import React, {createContext, useEffect} from 'react';
-import io, {Socket} from 'socket.io-client';
 import Config from 'react-native-config';
+import {useRecoilState} from 'recoil';
+import io, {Socket} from 'socket.io-client';
+import {LocationAtoms} from '../redux/atoms';
+import {useSession} from '../hooks';
 
 const {SOCKET_URL} = Config;
 interface SocketProviderProps {
@@ -16,13 +19,39 @@ export const SocketContext = createContext<SocketProviderContextModel>({
 
 const SocketProvider: React.FC<SocketProviderProps> = ({children}) => {
   const [socket, setSocket] = React.useState<Socket | null>(null);
+  const {user} = useSession();
+  const [_, setJoinedUsers] = useRecoilState(LocationAtoms.joinedUsers);
   useEffect(() => {
-    const socketObject = io(SOCKET_URL);
-    setSocket(socketObject);
-  }, []);
+    if (user) {
+      if (!socket?.connected) {
+        const socketObject = io(SOCKET_URL, {
+          transports: ['websocket'],
+          upgrade: false,
+        });
+        setSocket(socketObject);
+      }
+    }
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('users', data => {
+        setJoinedUsers(data);
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [socket]);
+
+  const connect = () => {
+    if (socket) {
+      socket.connect();
+    }
+  };
   const value = {
     socket,
+    connect,
   };
   return (
     <SocketContext.Provider value={value}>{children}</SocketContext.Provider>
