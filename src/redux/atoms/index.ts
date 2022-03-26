6a88dd15886773d25c024daece96c95076dcec75
tejas96 <tejas96patil@gmail.com>
2022-03-26 21:02:24 +0530

    add recoil atom for driver

--- src/redux/atoms/index.ts
+++ src/redux/atoms/index.ts
@@ -2,3 +2,4 @@ export * from './hospital';
 export * as SocketAtoms from './socket';
 export * as LocationAtoms from './location';
 export * as LoggedInUser from './loggedInUser';
+export * as Driver from './driver';
